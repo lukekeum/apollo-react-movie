331422e68508985b016d252832d84d577e7e2adf
lukekeum <lukekeum11@gmail.com>
2020-09-28 21:04:54 +0900

    #1 1 GET MOVIE Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,21 @@
-export default () => 'Home';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  if (loading) return 'loading...';
+  if (data && data.movies)
+    return data.movies.map((movie) => <li>{movie.id}</li>);
+};
+
+export default Home;
